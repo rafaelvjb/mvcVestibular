b897b126509c66a932a0b58e1984f983982a934a
Rafaelvjb <rafaelvjb@hotmail.com>
2013-04-16 11:36:08 -0300

    create a example of basic mvc - class video 02

--- mvcVestibular/mvcVestibular/Global.asax.cs
+++ mvcVestibular/mvcVestibular/Global.asax.cs
@@ -26,7 +26,7 @@ namespace mvcVestibular
             routes.MapRoute(
                 "Default", // Route name
                 "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+                new { controller = "Exemplo", action = "Index", id = UrlParameter.Optional } // Parameter defaults
             );
 
         }
